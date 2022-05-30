729f41a49f38aea6cdf858d9a0f3315b97608849
danivelarde <danielavelarde4@gmail.com>
2022-05-30 10:56:28 -0400

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <div className="footer">
+<footer className="bg-dark text-center text-white">
+  <div className="container p-4">
+    <section className="mb-4">
+      <a className="btn btn-outline-light btn-floating m-3" href="https://www.linkedin.com/in/daniela-velarde-8baa13141/" role="button" ><i className="fab fa-linkedin-in"></i></a>
+
+      <a className="btn btn-outline-light btn-floating m-3" href="https://github.com/davelarde" role="button" ><i className="fab fa-github"></i> </a>
+    </section>
+  </div>
+  <div class="footer-copyright text-center py-3">Daniela Velarde Professional Portfolio
+  </div>
+</footer>
+</div>
+  );
+};
+
+export default Footer;
