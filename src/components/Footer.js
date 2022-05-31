f53cad01b2b0c70fbdf226b3619f6df54dd29d8c
danivelarde <danielavelarde4@gmail.com>
2022-05-30 21:20:32 -0400

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import { SocialIcon } from 'react-social-icons';
 const Footer = () => {
   return (
       <div className="footer">
 <footer className="bg-black text-center text-white">
   <div className="container p-2">
     <section className="mb-4">
-      <a className="btn btn-outline-light btn-floating m-3 text-white" href="https://www.linkedin.com/in/daniela-velarde-8baa13141/" role="button" ><i className="fab fa-linkedin-in" target="_blank" rel="noopener noreferrer"></i>LINKEDIN</a>
-
-      <a className="btn btn-outline-light btn-floating m-3 text-white" href="https://github.com/davelarde" role="button" ><i className="fab fa-github"target="_blank" rel="noopener noreferrer"></i>GITHUB  </a>
-      <a className="btn btn-outline-light btn-floating m-3 text-white" href="mailto:danielavelarde4@gmail.com" role="button" ><i className="fab fa-EMAIL"target="_blank" rel="noopener noreferrer"></i>EMAIL ME ✉ </a>
+      <a className="fab fa-linkedin-in " href="url" ><SocialIcon url="https://www.linkedin.com/in/daniela-velarde-8baa13141/"  target="_blank" rel="noopener noreferrer"/></a>
+      <a className="fab fa-github-in" href="url" ><SocialIcon url="https://github.com/davelarde"  target="_blank" rel="noopener noreferrer"/></a>
+      <a className="fab fa-gmail-in" href="url" ><SocialIcon url="mailto:danielavelarde4@gmail.com"  target="_blank" rel="noopener noreferrer"/></a>
   <div class="footer-copyright text-center py-auto p-0">Daniela Velarde Professional Portfolio
   </div>
   <div class="footer-copyright text-center py-auto p-0">Made with ❤ 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar  = () => {
   </button> */
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
+    <ul className="navbar-nav ml-auto ">
     <li className="nav-item active">
     <Link to="/" className="nav-link">HOME</Link>
       </li>
